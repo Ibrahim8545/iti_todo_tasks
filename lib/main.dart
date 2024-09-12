ee204d0919824ac88ea85f30cbd8f2527cb2c59f
ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-09-13 02:35:08 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:todo_iti/views/home_page.dart';
+
+void main() {
+  runApp(ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
