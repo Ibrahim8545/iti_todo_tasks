e7cd77903bd5b2b86a811c13e595aa3d48c64ed6
ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-09-25 22:44:57 +0300

    update task model and main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:todoiti/home_page.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todoiti/models/panel_model.dart';
+import 'package:todoiti/models/view_model.dart';
 
-void main() {
-  runApp(ToDoApp());
-}
+import 'home_page.dart';
+import 'package:provider/provider.dart';
 
-class ToDoApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(PanelModelAdapter());
+  Hive.registerAdapter(TaskItemAdapter());
+  await Hive.openBox<PanelModel>('panels');
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => TaskViewModel(),
+      child: MaterialApp(home: HomePage()),
+    ),
+  );
 }
