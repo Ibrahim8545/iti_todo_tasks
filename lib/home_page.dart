9918e979147522c0b866318d8642caea6c5a63b3
ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-09-25 22:43:20 +0300

    update home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,128 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:todoiti/widgets/task_bottom_sheet.dart';
+import 'package:provider/provider.dart';
+import 'package:todoiti/models/view_model.dart';
 
-
-import 'utils/task_controller.dart';
+import 'widgets/task_bottom_sheet.dart';
 import 'widgets/expansion_panel.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Scaffold(
-          backgroundColor: const Color(0xfffefefe),
-          appBar: AppBar(
-            backgroundColor: const Color(0xfffefefe),
-            toolbarHeight: 100,
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Today',
-                  style: TextStyle(
-                    color: Color(0xff141414),
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      ' ${DateFormat.EEEE().format(DateTime.now())} , ${DateTime.now().day}, ${DateFormat.MMM().format(DateTime.now())} ${DateTime.now().year}',
-                      style: const TextStyle(
-                          color: Color(0xffc8c8c8), fontSize: 28),
-                    ),
-                    const SizedBox(width: 23),
-                    const Icon(Icons.calendar_today, color: Colors.grey),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          body: ListView.builder(
-            itemCount: TaskController.panels.length,
-            itemBuilder: (context, index) {
-              final panel = TaskController.panels[index];
-              return ExpansionPanelItem(
-                panel: panel,
-                onDeleteTask: (task) {
-                  setState(() {
-                    TaskController.deleteTask(panel, task);
-                    if (panel.items.isEmpty) {
-                      TaskController.deletePanel(panel);
-                    }
-                  });
-                },
+    final viewModel = Provider.of<TaskViewModel>(context);
 
-              );
+    return Scaffold(
+      appBar: AppBar(title: Text('Task Manager')),
+      body: ListView.builder(
+        itemCount: viewModel.panels.length,
+        itemBuilder: (context, index) {
+          final panel = viewModel.panels[index];
+          return ExpansionPanelItem(
+            panel: panel,
+            onDeleteTask: (task) {
+              viewModel.deleteTask(index, task);
+              if (panel.items.isEmpty) {
+                viewModel.deletePanel(index);
+              }
             },
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              elevation: 2,
-              side: const BorderSide(
-                color: Colors.white,
-                width: 2,
-                style: BorderStyle.solid,
-              ),
-              backgroundColor: Colors.black,
-              padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 10),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(22),
-              ),
-            ),
-            onPressed: () {
-              showModalBottomSheet(
-                isScrollControlled: true,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(35),
-                ),
-                context: context,
-                builder: (context) {
-                  return TaskBottomSheet(
-                    onAdd: (panel) {
-                      setState(() {
-                        TaskController.addPanel(panel);
-                      });
-                    },
-                  );
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return TaskBottomSheet(
+                onAdd: (panel) {
+                  viewModel.addPanel(panel);
                 },
               );
             },
-            child:const  Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  Icons.add,
-                  size: 30,
-                  color: Colors.white,
-                ),
-                Text(
-                  "Add Task",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
+          );
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
